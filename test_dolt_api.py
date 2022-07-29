235818f269aba548d3e9475e6bfbd49845fe6657
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-30 06:33:49 +0900

    select api

--- test_dolt_api.py
+++ test_dolt_api.py
@@ -2,3 +2,12 @@ import requests
 owner, repo = 'dolthub', 'nba-players'
 res = requests.get('https://dolthub.com/api/v1alpha1/{}/{}'.format(owner, repo))
 print(res.json())
+
+owner, repo, branch = 'dolthub', 'nba-players', 'master'
+query = '''SELECT tot.pts / tot.gp as ppg, ply.full_name as player
+    FROM `season_totals_regular_season` as tot
+    JOIN `players` as ply ON tot.player_id = ply.id
+    WHERE tot.season_id = '2019-20'
+    ORDER BY ppg DESC LIMIT 10;'''
+res = requests.get('https://www.dolthub.com/api/v1alpha1/{}/{}/{}'.format(owner, repo, branch), params={'q': query})
+print(res.json())
