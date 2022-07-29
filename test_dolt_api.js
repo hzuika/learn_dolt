b987697377d8ae8a60604877663d34fff865b7b9
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-30 07:26:02 +0900

    fix: 一行ごとに代入

--- test_dolt_api.js
+++ test_dolt_api.js
@@ -2,8 +2,10 @@
 
 const axios = require('axios').default;
 
-owner, repo, branch = 'dolthub', 'nba-players', 'master'
-query = "SELECT id FROM `players` where last_name='Harden'"
+let owner = 'dolthub'
+let repo = 'nba-players'
+let branch = 'master'
+let query = "SELECT id FROM `players` where last_name='Harden'"
 
 //res = await axios.get(`https://www.dolthub.com/api/v1alpha1/${owner}/${repo}`, {params:{q: query}})
 
