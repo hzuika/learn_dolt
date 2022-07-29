3fe49caf55754b82ca5dcf1504b5a3d03ef58277
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-30 06:47:43 +0900

    200ならjson表示

--- test_dolt_api.py
+++ test_dolt_api.py
@@ -11,7 +11,11 @@ query = '''SELECT tot.pts / tot.gp as ppg, ply.full_name as player
     ORDER BY ppg DESC LIMIT 10;'''
 res = requests.get('https://www.dolthub.com/api/v1alpha1/{}/{}/{}'.format(owner, repo, branch), params={'q': query})
 print(res) #504
-
+def print_res(res):
+    if res.status_code == requests.codes.ok
+        print(res.json())
+print_res(res)
 query = "SELECT id FROM `players` where last_name='Harden'"
 res = requests.get('http://www.dolthub.com/api/v1alpha1/{}/{}'.format(owner, repo), params={'q': query})
 print(res)
+print_res(res)
