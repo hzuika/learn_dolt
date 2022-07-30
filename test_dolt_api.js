e43185b22e3c03e8eafddca375b7886ba21e8a39
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-30 11:27:29 +0900

    Update test_dolt_api.js

--- test_dolt_api.js
+++ test_dolt_api.js
@@ -9,6 +9,14 @@ let query = "SELECT id FROM `players` where last_name='Harden'"
 
 //res = await axios.get(`https://www.dolthub.com/api/v1alpha1/${owner}/${repo}`, {params:{q: query}})
 
+const url = "https://www.dolthub.com/api/v1alpha1" 
 axios.get(`https://www.dolthub.com/api/v1alpha1/${owner}/${repo}`, {params:{q: query}}).then((res) => {
 console.log(res)
 })
+
+owner = 'hzuika'
+repo = 'dotlive_video'
+query = "SELECT * FROM channel"
+axios.get(`${url}/${owner}/${repo}`, {params:{q: query}}).then(res => {
+  console.log(res)
+}
